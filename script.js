ccf2e5c093c708961af881696f81ec2e799cae06
Toushif468 <toushiforbeebro@gmail.com>
2024-06-07 22:15:18 +0600

    login(javascript)

---
+++ script.js
@@ -0,0 +1,9 @@
+let signUpBtn = document.querySelector('.Signupbtn');
+let signInBtn = document.querySelector('.Signinbtn');
+let nameField = document.querySelector('.namefield');
+let title = document.querySelector('.title');
+let underline = document.querySelector('.underline');
+
+signInBtn.addEventListener('click',()=>{
+    nameField.style.maxHeight = '0';
+});
